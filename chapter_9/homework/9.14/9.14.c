ea4e9f1029161f00a5676eaacf13862547b136fc
DesmondoRay <desmondorayy@gmail.com>
2018-08-28 16:50:36 +0800

    homework 9.14~9.16

---
+++ chapter_9/homework/9.14/9.14.c
@@ -0,0 +1,25 @@
+/**
+ * homework 9.14
+ *
+ * 使用mmap将hello.txt的内容改变为"Jello, world!\n"。
+ */
+
+#include "../../code/csapp.h"
+
+int main(void)
+{
+	int fd;
+	struct stat st;
+	char *bufp;
+	
+	fd = Open("hello.txt", O_RDWR, 0);
+	fstat(fd, &st);
+	
+	bufp = Mmap(NULL, st.st_size, PROT_WRITE, MAP_PRIVATE, fd, 0);
+	*bufp = 'J';
+	
+	Write(1, bufp, st.st_size);
+	Munmap(bufp, st.st_size);
+	
+	exit(0);
+}
