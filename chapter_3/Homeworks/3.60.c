ca77353350e3c5a3ba8095fac8aa855346b23399
evanchime <evanchime@yahoo.co.uk>
2019-09-05 11:10:38 +0100

    Modified Question D and updating of mask

--- chapter_3/Homeworks/3.60.c
+++ chapter_3/Homeworks/3.60.c
@@ -4,7 +4,7 @@
  * A. x in %rdi, n in %esi, result in %rax, mask in %rdx;
  * B. 初始值: result = 0, mask = 1;
  * C. mask != 0;
- * D. mask = mask << n;
+ * D. mask = mask << (n & 0xFF);
  * E. (mask&x) in %r8, result = result | %r8;
  */
  
@@ -12,7 +12,7 @@ long loop(long x, int n)
 {
     long result = 0;
     long mask;
-    for (mask = 1; mask != 0; mask = mask << n) {
+    for (mask = 1; mask != 0; mask = mask << (n & 0xFF)) {
         result |= mask&x;
     }
     return result;
